f23590109352a192d9d6fa518e68ca143ed14bd8
Fred He <fhe@bayviewglen.ca>
2025-05-13 00:07:31 -0400

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,38 @@
+export default function sitemap() {
+  const baseUrl = 'https://www.reponote.org';
+  
+  const routes = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/docs`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/#features`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/#steps`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/#pricing`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+  ];
+
+  return routes;
+} 
